477d3c4525279c934a83af4b34d6d58365badd0e
mikhlasnr <minrnullpo@gmail.com>
2023-06-20 12:08:23 +0700

    implement icon to navigation

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, FlatList, SafeAreaView } from "react-native";
+import { FlatList } from "react-native";
 import styled from "styled-components/native";
-
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
-
+import { SafeArea } from "../../../components/utility/safe-area.component";
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
+  padding-bottom: 0;
+  padding-top: 0;
+
+  /* background-color: ${(props) => props.theme.colors.bg.primary}; */
 `;
 
 const RestaurantList = styled(FlatList).attrs({
@@ -24,7 +22,7 @@ const RestaurantList = styled(FlatList).attrs({
 export const RestaurantsScreen = () => (
   <SafeArea>
     <SearchContainer>
-      <Searchbar />
+      <Searchbar style={{ backgroundColor: "#FFFF" }} placeholder="Search" />
     </SearchContainer>
     <RestaurantList
       data={[
