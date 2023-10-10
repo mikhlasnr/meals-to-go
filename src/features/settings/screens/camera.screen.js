2f72ba1a3dfdd336359679cf0a422c3d366bb3d2
ikhlaspaperpillar <ikhlas@paperpillar.com>
2023-10-10 13:22:17 +0700

    create camera photo

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,12 +1,30 @@
 import React, { useRef, useState, useEffect } from "react";
 import { Camera, CameraType } from "expo-camera";
 import styled from "styled-components/native";
-import { View } from "react-native";
+import { View, TouchableOpacity } from "react-native";
 import { Text } from "../../../components/typography/text.component";
+import { Button } from "react-native-paper";
 
 const ProfileCamera = styled(Camera)`
   width: 100%;
   height: 100%;
+`;
+
+const CameraContainer = styled.View`
+  width: 100%;
+  height: 100%;
+`;
+
+const CameraButton = styled(Button).attrs({
+  mode: "contained",
+  icon: "camera",
+})`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  justify-content: center;
+  border-radius: 0;
+  height: 48px;
 `;
 
 export const CameraScreen = () => {
@@ -15,10 +33,17 @@ export const CameraScreen = () => {
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === "granted");
+      const { granted } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(granted);
     })();
   }, []);
+
+  const snap = async () => {
+    if (cameraRef && cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      console.log(photo);
+    }
+  };
 
   if (hasPermission === null) {
     return <View />;
@@ -26,10 +51,27 @@ export const CameraScreen = () => {
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
+
   return (
-    <ProfileCamera
-      ref={(camera) => (cameraRef.current = camera)}
-      type={CameraType.front}
-    />
+    <CameraContainer>
+      <ProfileCamera
+        ref={(camera) => (cameraRef.current = camera)}
+        ratio={"16:9"}
+        type={CameraType.front}
+        onCameraReady={() => {
+          console.log("Camera Ready");
+        }}
+      ></ProfileCamera>
+      <CameraButton onPress={snap}>Snap!</CameraButton>
+    </CameraContainer>
   );
+
+  // return (
+  //   <TouchableOpacity onPress={snap}>
+  //     <ProfileCamera
+  //       ref={(camera) => (cameraRef.current = camera)}
+  //       type={CameraType.front}
+  //     />
+  //   </TouchableOpacity>
+  // );
 };
